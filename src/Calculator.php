c60e670281dc00c70945e495ce52b3a1cadf8b6c
Julie <raymondjulie@proton.me>
2023-11-11 17:30:11 +0100

    "exercice done"

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,8 +4,22 @@ namespace App;
 
 class Calculator
 {
-    public function sum(int $firstValue, int $secondValue ): int 
+    public function sum(int $firstValue, int $secondValue): int
     {
         return $firstValue + $secondValue;
+    }
+
+    public function multiplication(int $firstnumber, int $secondnumber): int
+    {
+        return $firstnumber * $secondnumber;
+    }
+
+    public function isEven(int $number) :bool
+    {
+        if ($number % 2 == 0){
+            return true;
+            }else{
+            return false;
+            }
     }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -16,10 +16,49 @@ class CalculatorTest extends TestCase
         //Act
         $calculator = new Calculator();
         $sum = $calculator->sum($firstValue, $secondValue);
-        
+
         //Assert
-        $this->assertSame($expectedResult, $sum, 
+        $this->assertSame(
+            $expectedResult,
+            $sum,
             "La méthode sum() ne fonctionne plus :-("
+        );
+    }
+
+    public function testmultiplication(): void
+    {
+        //Arrange
+        $firstNumber = 2;
+        $secondNumber = 5;
+        $expectedNumber = 10;
+
+        //Act
+        $calculator = new Calculator();
+        $multiplication = $calculator->multiplication($firstNumber, $secondNumber);
+
+        //Assert
+        $this->assertSame(
+            $expectedNumber,
+            $multiplication,
+            "La méthode multiplication() ne fonctionne plus :-("
+        );
+    }
+
+    public function testIsEven(): void
+    {
+        //Arrange
+        $number = 2;
+        $expectedBool = true;
+
+        //Act
+        $calculator = new Calculator();
+        $isEven = $calculator->isEven($number);
+
+        //Assert
+        $this->assertSame(
+            $expectedBool,
+            $isEven,
+            "La méthode isEven() ne fonctionne plus :-("
         );
     }
 }
